b72c1e2a3ae21343dd0832dbf36b5f3dfa241ad4
Hà Tiến Đông <125755217+phapsucongu@users.noreply.github.com>
2024-10-19 20:34:15 +0700

    Delete week3 directory

--- week3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.week3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.junit.jupiter.api;
-
-    opens com.example.week3 to javafx.fxml;
-    exports com.example.week3;
-}
